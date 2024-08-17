bc99fb124b955bf577daad33e67725fa0578fda7
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-18 02:43:34 +0530

    generated file: `utils/constants.js`

---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+export const API_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+export const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+export const STORAGE_BUCKET = 'your-supabase-storage-bucket'; // Replace with your bucket name
+export const DEFAULT_AVATAR_URL = '/profile-placeholder.jpg';
+export const GOAL_STATUS = {
+  ACTIVE: 'Active',
+  COMPLETED: 'Completed',
+  IN_PROGRESS: 'In Progress',
+};
